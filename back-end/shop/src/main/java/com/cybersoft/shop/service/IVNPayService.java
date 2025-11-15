3353f2e80082f7b4148ec9e9cff7a1bf530f9b2b
Nguyen Chi Cuong <nguyenchicuong11022003@gmail.com>
2025-11-15 18:12:11 +0700

    Ipn & refund

--- back-end/shop/src/main/java/com/cybersoft/shop/service/IVNPayService.java
+++ back-end/shop/src/main/java/com/cybersoft/shop/service/IVNPayService.java
@@ -1,11 +1,16 @@
 package com.cybersoft.shop.service;
 
 import com.cybersoft.shop.request.PaymentRequest;
+import com.cybersoft.shop.request.QueryRequest;
+import com.cybersoft.shop.request.RefundRequest;
 import jakarta.servlet.http.HttpServletRequest;
 
 import java.util.Map;
 
 public interface IVNPayService {
     String createPaymentUrl(PaymentRequest paymentRequest, HttpServletRequest request);
-    String handleVnpayIPN(HttpServletRequest request);
+    Map<String, String> handleVnpayIPN(HttpServletRequest request);
+    Map<String, Object> queryDr(QueryRequest req, HttpServletRequest servletReq);
+    Map<String, Object> refund(RefundRequest req, HttpServletRequest servletReq);
+
 }
